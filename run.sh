17fbb0616a2ea4ec332528696254ee88aa918cad
jessMan98 <54372173+jessMan98@users.noreply.github.com>
2021-11-11 12:48:42 -0600

    Actualización de script

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
-export SOFTWARE=/home/neutrino/software
-source $SOFTWARE/root_v5.34.38/install/bin/thisroot.sh
+export SOFTWARE=/home/manuel/software
 export GEANT4_BASE_DIR=$SOFTWARE/geant4.10.01.p03/install
 export G4WORKDIR=$PWD
 
@@ -9,5 +8,6 @@ cd $SOFTWARE/WCSim_build
 
 source $SOFTWARE/geant4.10.01.p03/install/bin/geant4.sh
 source $SOFTWARE/geant4.10.01.p03/install/share/Geant4-10.1.3/geant4make/geant4make.sh
+source $SOFTWARE/root_v5.34.38/install/bin/thisroot.sh
 export WCSIMDIR=$PWD
 export G4WORKDIR=$PWD
